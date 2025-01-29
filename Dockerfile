ff37d3e14cc026a491fad256e12abe2a4a6a291a
ひろ <koki.hiro0723@gmail.com>
2025-01-29 14:53:50 +0900

    GORMを使って、PostgreSQLに接続

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ WORKDIR /app
 COPY src/go.mod .
 COPY src/go.sum .
 RUN go mod download
+
+COPY src .
+CMD ["go", "run", "main.go"]
--- src/main.go
+++ src/main.go
@@ -3,15 +3,37 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	dsn := "host=postgres_db user=root password=password dbname=dev port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1) // find product with integer primary key
+
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "success",
+			"code": product,
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
